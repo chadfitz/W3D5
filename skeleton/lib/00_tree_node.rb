2c10a9340606aa578141deab299ecd94a0b21dc9
chad fitzgerald <fitzchad@gmail.com>
2022-10-21 11:46:30 -0700

    Finish phase 1

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,29 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = Array.new
+    end
 
+    def parent=(node)
+        return @parent = nil if node == nil
+        return  if node.children.include?(self)
+        @parent = node
+        node.children << self
+    end
+
+    # def parent
+    #     if @parent
+    #         return @parent
+    #     else
+    #         return nil
+    #     end
+    # end
+
+    def inspect
+        # { 'value' => @value, 'parent_value' => @parent.value }.inspect 
+        { 'value' => @value }.inspect 
+    end
 end
