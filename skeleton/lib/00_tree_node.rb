e46d5b8e26c24acaf8f15daf1ee2d66cad93ccf7
ByronHan333 <zhan12@dons.usfca.edu>
2022-10-21 12:10:09 -0700

    phase 2,3

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,6 +1,6 @@
 class PolyTreeNode
     attr_reader :parent, :children, :value
-    
+
     def initialize(value)
         @value = value
         @parent = nil
@@ -8,22 +8,50 @@ class PolyTreeNode
     end
 
     def parent=(node)
-        return @parent = nil if node == nil
-        return  if node.children.include?(self)
-        @parent = node
-        node.children << self
+        if @parent == nil
+            if node == nil
+                return @parent = nil
+            else
+                @parent = node
+                @parent.children << self if !@parent.children.include?(self)
+                return
+            end
+        else
+            if node == nil
+                return @parent = nil
+            else
+                old_parent = @parent
+                old_parent.children.delete(self)
+                @parent = node
+                @parent.children << self if !@parent.children.include?(self)
+                return
+            end
+        end
     end
 
-    # def parent
-    #     if @parent
-    #         return @parent
-    #     else
-    #         return nil
-    #     end
-    # end
+    def add_child(child_node)
+        self.children << child_node
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "node is a child" if !self.children.include?(child_node)
+        self.children.delete(child_node)
+        child_node.parent = nil
+    end
 
     def inspect
-        # { 'value' => @value, 'parent_value' => @parent.value }.inspect 
-        { 'value' => @value }.inspect 
+        { 'value' => @value, 'parent_value' => @parent.value }.inspect
+        # { 'value' => @value }.inspect
     end
 end
+
+
+# parent node p1, p2
+# child node c (current)
+
+# case 1: c.parent = nil
+    # subcase 1: node given is nil
+    # subcase 2: node given is not nil
+
+# case 2: c.parent is not nil
