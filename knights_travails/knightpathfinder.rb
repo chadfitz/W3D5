5200d6b264785a15a5ae97f0e4cc6535dd69ef6c
chad fitzgerald <fitzchad@gmail.com>
2022-10-21 14:51:47 -0700

    Finish phase 1 knights travails

--- knights_travails/knightpathfinder.rb
+++ knights_travails/knightpathfinder.rb
@@ -1,5 +1,5 @@
 require_relative "../skeleton/lib/00_tree_node"
-
+require "byebug"
 class KnightPathFinder
 
   def self.root_node(pos)
@@ -8,35 +8,56 @@ class KnightPathFinder
 
   def self.valid_moves(pos)
     arr = []
-    row, col = pos
+    row, col = pos.value
     arr << [row+2, col+1]
-    ...
-    ..
-    ..
+    arr << [row+2, col-1]
+    arr << [row+1, col-2]
+    arr << [row+1, col+2]
+    arr << [row-2, col+1]
+    arr << [row-2, col-1]
+    arr << [row-1, col+2]
+    arr << [row-1, col-2]
 
-    arr.select {|r,c| r <=7 && r >= 0}
-
-    end
-    [[],[]]
+    moves = arr.select {|r,c| r <=7 && r >= 0 && c >= 0 && c <= 7}
+    moves.map {|r,c| PolyTreeNode.new([r,c])}
   end
 
-  attr_accessor :considered_position
+  attr_accessor :considered_positions
+  attr_reader :position
 
   def initialize(pos)
-    @pos = KnightPathFinder.root_node(pos)
-    @considered_position = [pos]
+    @position = KnightPathFinder.root_node(pos)
+    @considered_positions = [@position]
     # self.build_move_tree(self.root_node)
   end
 
   def new_move_positions(pos)
-
+    all_possible_pos = KnightPathFinder.valid_moves(pos)
+    # all_possible_pos.reject { |old_pos| considered_positions.include?(old_pos) }
+    all_possible_pos.select { |pp| !considered_positions.include?(pp) }
   end
 
   def build_move_tree
-
-
+    arr = []
+    arr << self.position
+    until arr.empty?
+      debugger
+      current_node = arr.shift
+      considered_positions << current_node
+      new_move_positions(current_node).each do |mp|
+        arr << mp
+      end
+    end
+    arr
   end
 end
-
-# ptn = PolyTreeNode.new(3)
-# print ptn
+    # def bfs(target_value)
+    #     arr = []
+    #     arr.unshift(self)
+    #     until arr.empty?
+    #         node = arr.pop
+    #         return node if node.value == target_value
+    #         node.children.each {|child| arr.unshift(child)}
+    #     end
+    #     nil
+    # end
