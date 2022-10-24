6601aebd5ce5efbfb52065ef87bc164869b351b1
chadfitz <fitzchad@gmail.com>
2022-10-24 12:01:26 -0700

    Finish knights travails Start tictactoe

---
+++ tic_tac_toe/lib/tic_tac_toe_node.rb
@@ -0,0 +1,28 @@
+require_relative 'tic_tac_toe'
+
+class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_mov_pos = prev_mov_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+    res = []
+    (0...board.rows.length).each do |row|
+      (0...board.rows.length).each do |col|
+      
+      end
+    end
+  end
+end
