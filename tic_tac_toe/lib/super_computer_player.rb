0f7eeeb3c3910a997a9c57da2e3a7876b128a7c9
chadfitz <fitzchad@gmail.com>
2022-10-24 15:04:22 -0700

    finish tictactoe

--- tic_tac_toe/lib/super_computer_player.rb
+++ tic_tac_toe/lib/super_computer_player.rb
@@ -2,6 +2,16 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    t = TicTacToeNode.new(game.board, mark, prev_move_pos = nil)
+    t.children.each do |c|
+      if c.winning_node?(mark)
+        return c.prev_move_pos 
+      end
+    end
+    not_losing_nodes = t.children.select do |c|
+      !c.losing_node?(mark)
+    end
+    return not_losing_nodes.first.prev_move_pos
   end
 end
 
